479e022d1fca9bc2cc24fbd4e1c7a2b771aeb4a2
Yuzhe Zhang <zhangyuzhe0737@gmail.com>
2024-09-23 17:54:38 -0400

    Update App.js

--- project-manager/src/App.js
+++ project-manager/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editted!! <code>src/App.js</code> and save to reload.
+          Try this again<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
